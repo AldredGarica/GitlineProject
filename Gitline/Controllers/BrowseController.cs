a37d01167bd53dd58508d088d1fc76ffcda0b541
DreD <aldredrei.garcia@benilde.edu.ph>
2021-04-10 01:13:55 +0800

    Add Browse page

---
+++ Gitline/Controllers/BrowseController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Gitline.Data;
+using Gitline.Models;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+
+
+namespace Gitline.Controllers
+{
+    public class BrowseController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+        public BrowseController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var list = _context.Inventory.ToList();
+            return View(list);
+        }
+    }
+}
